9e61b76a382c67d5add034eb4796b71dbdb3a7f8
Thiago Aguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2025-11-17 17:58:10 -0300

    implementacion  de encontrar_componentes_conexos

--- tp-matdis-rpazos98-version-entrega/src/electricAlgorithms.py
+++ tp-matdis-rpazos98-version-entrega/src/electricAlgorithms.py
@@ -1,2 +1,37 @@
 ## Redes conexas
 ## grado de cada nodo
+
+
+def encontrar_componentes_conexos(graph):
+    """
+     Encuentra todos los componentes conexos de un grafo 
+     
+     Args: toma como argumento un diccionario de  clave nodo valor lista de adyacencia
+
+     Lista de listas donde cada lista son los nodos conexos
+     
+     """
+
+    visitados = set()
+    componentes = []
+
+    for nodo in graph:
+        if nodo not in visitados:
+            componente_actual = []
+            pila = [nodo]
+            visitados.add(nodo)
+
+            while pila:
+                u = pila.pop()
+                componente_actual.append(u)
+
+                # Recorrer vecinos
+                for v in graph.get(u, []):
+                    if v not in visitados:
+                        visitados.add(v)
+                        pila.append(v)
+
+            componente_actual.sort()
+            componentes.append(componente_actual)
+
+        return componentes
